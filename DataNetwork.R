77f9eb2716ff5abea4f2a8eec91794aad68c206e
karina7272 <31832684+karina7272@users.noreply.github.com>
2018-05-19 14:50:33 -0400

    Delete DataNetwork.R

--- DataNetwork.R
+++
@@ -1,8 +0,0 @@
-library("networkD3")
-
-setwd("C:/Users/karina72/Desktop")
-networkData <- read.csv(file="c:/course-proj-data.csv", header = T, sep = ";")
-
-head(networkData)
-simpleNetwork(networkData)
-
